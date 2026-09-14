674169cc80094345ceb9fb2d77604d2ac26b6f79
mrabbit2002-dotcom <mrabbit2002@gmail.com>
2026-09-14 14:20:42 +0900

    Add D20 Dice dock interface

---
+++ d20-dice/src/d20-dice-dock.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <QLabel>
+#include <QPushButton>
+#include <QWidget>
+
+class D20DiceDock : public QWidget {
+  Q_OBJECT
+public:
+  explicit D20DiceDock(QWidget *parent = nullptr);
+
+private:
+  QLabel *resultLabel{};
+  QPushButton *rollButton{};
+  QPushButton *visibilityButton{};
+
+  void roll();
+  void toggleVisibility();
+  void updateVisibilityButton();
+  bool ensureDiceSource(int result, qint64 nonce);
+};
